b8847b4dc8be2b7afc643e8b905cd5c4c5a2501a
ayushkush01 <atulsinghxs@gmail.com>
2026-07-22 10:16:02 +0530

    isshue sorted

---
+++ asquare_v2/ConfigKey.cs
@@ -0,0 +1,13 @@
+﻿namespace sqa_core.Configuration
+{
+    public static class ConfigKey
+    {
+        public static class Aws
+        {
+            public const string AccessKey = "THISISMYTESTKEYS";
+            public const string SecretKey = "THISISMYTESTKEYS";
+            public const string Region = "THISISMYTESTKEYS";
+            public const string BucketName = "THISISMYTESTKEYS";
+        }
+    }
+}
